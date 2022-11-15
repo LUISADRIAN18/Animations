96772efd8730743b348f814fb612156c0db29c0d
Adrian <luisadrian@gmail.com>
2022-11-14 20:33:55 -0800

    commit final

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -6,14 +6,84 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
+    var ani : LottieAnimationView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func didChangeSegment(_ sender: UISegmentedControl){
+        
+        switch sender.selectedSegmentIndex{
+        case 0:
+            view.backgroundColor = .yellow
+            ani?.stop()
+            ani = LottieAnimationView(name: "smile")
+            ani?.frame = self.view.frame
+            
+            ani?.animationSpeed = 0.2
+            ani?.loopMode = .playOnce
+            self.view.addSubview(ani!)
+            
+            ani?.play{(finis) in
+                self.ani?.isHidden = true
+                
+                
+            }
+            
+            
+            
+            
+            
+        case 1:
+            
+            view.backgroundColor = .blue
+            ani = LottieAnimationView(name: "button")
+            ani?.frame = self.view.frame
+            //ani?.animationSpeed = 0.2
+            ani?.loopMode = .playOnce
+            self.view.addSubview(ani!)
+            
+           
+            ani?.play{(finis) in
+                self.ani?.isHidden = true
+                
+                
+            }
+                      
+            
+           
+            
+        case 2:
+            view.backgroundColor = .green
+            ani = LottieAnimationView(name: "allin")
+            ani?.frame = self.view.frame
+            //ani?.animationSpeed = 0.2
+            ani?.loopMode = .playOnce
+            self.view.addSubview(ani!)
+            
+           
+            ani?.play{(finis) in
+                self.ani?.isHidden = true
+                
+                
+            }
+        
+            
+            
+            
+        default:
+            view.backgroundColor = .yellow
+        }
+        
+    }
 
+    
 
 }
 
